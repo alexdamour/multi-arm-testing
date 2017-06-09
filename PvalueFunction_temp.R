6affd15894d2f89221ea6e33a3380c7ee8aae252
Yuxin Ma <Yuxin@anhuideMacBook-Air.local>
2017-06-08 17:43:25 -0700

    New update 06081743

---
+++ PvalueFunction_temp.R
@@ -0,0 +1,130 @@
+#Fundamental Simulation Function
+##Generate p-value function
+compute_pvalue = function(Yk, sigk, N, K){
+  n <- N/K
+  nk = rep(n, K) # assume evenly distributed number of subgroups nk
+  # initialize the parameters
+  tao = NULL # causal estimand
+  Yob = NULL
+  sig = NULL
+  V = NULL # neyman estimators of variance
+  t = NULL # t-statiscs
+  # p-value matrix
+  p = matrix(0, ncol = K, nrow = n)
+  # gen YK obs 
+  for (j in 1:n){
+    for (i in 1:K){ 
+      # derive the sample mean and sample variance 
+      Yob[i] = rnorm(1, Yk[i], sigk[i] * sqrt(K/N))
+      sig[i] = sigk[i] * rchisq(1, nk[i] - 1) / (nk[i] - 1)
+      Y0 = Yob[1] # non-treat mean
+      tao[i] = Yob[i] - Y0 
+      V[i] = sig[i]/nk[i] + sig[1]/nk[1]
+      # calculate the t-statistics
+      t[i] = (tao[i] - 0) / sqrt(V[i])
+      # get the two-sided p-value
+      p[j,i] = (1 - pt(abs(t[i]), nk[i] - 2)) * 2
+    }
+  }
+  return(p)
+}
+
+
+##Generate p-combine function
+compute_pcombine <- function(Yk, sigk, N, K, ptype){
+  n <- N/K
+  p_comb_val <- NULL
+  pmatrix <- compute_pvalue(Yk, sigk, N, K)
+  for (k in 1:n){
+    if (ptype == "min"){
+      p_comb_val[k] = min(pmatrix[k,-1])
+    }else{
+      if (ptype == "max"){
+        p_comb_val[k] = max(pmatrix[k,-1])  
+      }else{
+        if (ptype == "median"){
+          p_comb_val[k] = median(pmatrix[k,-1])
+        }else{
+          if (ptype == "mean"){
+            p_comb_val[k] = mean(pmatrix[k,-1])
+          }else{
+            print("Error: not choosing the existent type")
+          }
+        }
+      }
+    }
+  }
+  return (p_comb_val)
+}
+
+
+##Generate power function
+compute_power <- function(Yk, sigk, N, K, p_cv, ptype){
+  n <- N/K
+  power = 0
+  pcomb = compute_pcombine(Yk, sigk, N, K, ptype)
+  rejec = 0
+  rec = 0
+  for (i in 1:n){
+    if(pcomb[i] <= p_cv){
+      rejec = rejec + 1
+    }else{
+      rec = rec + 1
+    }
+  }
+  power = rejec / (rejec + rec)
+  return(power)
+}
+
+##80% Power critical ratio calculating function
+compute_ctratio = function(Y, N, K, r1_start = 0.01, r1_step =0.001, r1_ceil = 1, rk = 1){
+  # set the starting critical value
+  r1_critical = r1_start
+  
+  power = ratio_power(Y, r1_critical, rk, N, K, compute_pcombine, compute_power)
+  if (r1_start > r1_ceil){
+    print("Error: critical value does not exist")
+  }else{
+    r1_start = r1_start + r1_step
+    power_ahead = ratio_power(Y, r1_start, rk, N, K, compute_pcombine, compute_power)
+    if (power < 0.8 | power >= power_ahead){
+      return(compute_ctratio(Y, n, K, r1_start = r1_start, r1_step = r1_step, r1_ceil = r1_ceil, rk = rk))
+    }else{
+      return (r1_critical)  
+    } 
+  }  
+}
+
+##Binary critical ratio calculation
+compute_binary_ctratio = function(Y, N, K, delta, r1_start = 0, r1_step = 0.15, r1_ceil = 1, rk = 1){
+  critical = (r1_start + r1_ceil)/2
+  
+  power = ratio_power(Y, critical, rk, N, K, compute_pcombine, compute_power)
+  
+  # check for the monotonicity
+  critical_next = critical + r1_step
+  
+  power_ahead = ratio_power(Y, critical_next, rk, N, K, compute_pcombine, compute_power)
+  diff = abs(power - 0.8)
+  while (diff > delta) {
+    print(diff)
+    print(power)
+    print(critical)
+    if (r1_start == r1_ceil ){
+      ## avoid infinite loop case, reduce the floor r1_start
+    critical = r1_start/2
+    return(compute_binary_ctratio(Y, N, K, delta = delta, r1_start = critical, r1_step = r1_step, r1_ceil = r1_ceil, rk = rk))
+    }else{
+    if (power > power_ahead | power < 0.8 ){
+      return(compute_binary_ctratio(Y, N, K, delta = delta, r1_start = critical, r1_step = r1_step, r1_ceil = r1_ceil, rk = rk))
+    }else{
+      if (power > 0.8){
+        return(compute_binary_ctratio(Y, N, K, delta = delta, r1_start = r1_start, r1_step = r1_step, r1_ceil = critical, rk = rk))
+      }else{
+        print("Error: does not exists")
+        }
+      }   
+    }
+  }  
+  return(critical)
+}
---
+++ RatioPower_temp.R
@@ -0,0 +1,145 @@
+# define a function that returns the power value given 
+# ratio r1 = y1/sig1 and ratio rk = sigk/sig1
+ratio_power = function(Y, r1, rk, N, K, compute_pcombine, compute_power, knum = 1, ktype = "homo", ptype = "min"){
+  # the treatment level y1
+  y1 = Y[2]
+  sig1 = y1/r1
+  # derive the null mean Y_null
+  Y_null = rep(0, K)
+  
+  # derive the standard deviation sig array 
+  sigk = NULL
+  if (ktype == "homo"){
+    sigk = rep(rk* y1/r1, K)
+  }else{
+    if (ktype == "rk-hetero"){
+      sigk = rk * y1/r1
+    }else{
+      if(ktype == "sig-hetero"){
+        sigk_rest = exp(rnorm(K-1, log(rk*sig1)))
+        sigk = c(sigk_rest[1], 0, sigk_rest[2:K-1])     
+      }else{
+        print("Error: not choosing the existent ktype-type")
+      }
+    }
+  }
+  
+  sigk[2] = y1/r1 
+  
+  # compute the corresponding p_power 
+  power = NULL
+  # derive the critical value p_cv for null hypothesis
+  p_cv = quantile(compute_pcombine(Y_null, sigk, N, K, ptype), 0.05)
+  # get the sparse effect 
+  
+  for (j in 1:knum){
+    power[j] = compute_power(Y, sigk, N, K, p_cv, ptype)
+  }
+  avg_power = mean(power)
+  
+  return(avg_power) 
+}
+
+# minimal sigma case 
+sig_stat_gen = function(mean, K, ratio_type = "min", sig_type = "uniform"){
+  if (sig_type == "uniform"){
+    rk = runif(K, max = 2 * mean)
+  }else{
+    if (sig_type == "log-normal"){
+      rk = exp(rnorm(K, log(mean)))
+    }else{
+      print("Error: not choosing the existent sig-type")
+    }
+  }
+  # get the rest of sigmas apart from sig1
+  rk_rest = c(rk[1], rk[3:K])
+  
+  if (ratio_type == "min"){
+    rk_item = min(rk_rest)
+  }else{
+    if (ratio_type == "max" ){
+      rk_item = max(rk_rest)  
+    }else{
+      if (ratio_type == "median"){
+        rk_item = median(rk_rest)
+      }else{
+        if (ratio_type == "mean"){
+          rk_item = mean(rk_rest)
+        }else{
+          print("Error: not choosing the existent type")
+        }
+      }
+    }
+  }
+  return( list(rk_item = rk_item,  rk = rk) )
+}
+
+power_ratio_test = function(Y, N, K, n_r1, n_rk, ktype = "homo", knum = 1, ratio_type, sig_type, ratio_rep = 50, step_r1 = 0.1, step_rk = 0.1){
+  
+  power_matrix= matrix(0, nrow = n_r1, ncol = n_rk)
+  r1_item = NULL
+  rk_item = NULL
+  
+  if (ktype == "homo" | ktype == "sig-hetero"){
+    for (i in 1:n_r1){ # Row
+      r1_i = i * step_r1
+      r1_item[i] = r1_i
+      for (j in 1:n_rk){ # Column
+        rk_j = j * step_rk
+        rk_item[j] = rk_j
+        power_matrix[i,j] = ratio_power(Y, r1_i, rk_j, N, K, compute_pcombine, compute_power, ktype = ktype) 
+      }
+    }
+  }else{
+    if (ktype == "rk-hetero"){
+      for (i in 1:n_r1){
+        r1_i = i * step_r1
+        r1_item[i] = r1_i
+        ratio_exp = NULL
+        for (j in 1:n_rk){ # Column
+          mean_j = j * step_rk
+          rk_item_exp = NULL
+          power_exp = NULL
+          for (e in 1:ratio_rep){ # repeated experiments
+            statis_exp = sig_stat_gen(mean_j, K, ratio_type = ratio_type, sig_type = sig_type)
+            # get the rk_item sample
+            rk_item_exp[e] = statis_exp$rk_item 
+            # get the corresponding power
+            rk_exp = statis_exp$rk
+            power_exp[e] = ratio_power(Y, r1_i, rk_exp, N, K, compute_pcombine, compute_power, ktype = ktype, knum = knum)
+          }
+          # get the mean of rk_item and power for repeated experiments
+          rk_item[j] = mean(rk_item_exp)  
+          power_matrix[i,j] = mean(power_exp)}
+      }
+    }else{
+      if (ktype == "rk-hetero"){
+        for (i in 1:n_r1){
+          r1_i = i * step_r1
+          r1_item[i] = r1_i
+          ratio_exp = NULL
+          for (j in 1:n_rk){ # Column
+            mean_j = j * step_rk
+            rk_item_exp = NULL
+            power_exp = NULL
+            for (e in 1:ratio_rep){ # repeated experiments
+              statis_exp = sig_stat_gen(mean_j, K, ratio_type = ratio_type, sig_type = sig_type)
+              # get the rk_item sample
+              rk_item_exp[e] = statis_exp$rk_item 
+              # get the corresponding power
+              rk_exp = statis_exp$rk
+              power_exp[e] = ratio_power(Y, r1_i, rk_exp, n, K, compute_pcombine, compute_power, ktype = ktype, knum = knum)
+            }
+            # get the mean of rk_item and power for repeated experiments
+            rk_item[j] = mean(rk_item_exp)  
+            power_matrix[i,j] = mean(power_exp)
+          }
+        }
+      }else{
+        print("Error: not choosing the appropriate ktype")
+      }
+    }
+  }
+  return( list(power_matrix = power_matrix, rk = rk_item, r1 = r1_item) )
+}
+
